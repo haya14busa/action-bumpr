81228a3b12a7c3033dbfe64fd2bcf16552e76946
haya14busa <haya14busa@gmail.com>
2020-01-20 14:38:16 +0000

    Support bumpr with non-existing tags

--- entrypoint.sh
+++ entrypoint.sh
@@ -39,7 +39,25 @@ fi
 echo "Bump ${BUMP_LEVEL} version"
 
 git fetch --tags # Fetch existing tags before bump.
-NEXT_VERSION=$(bump ${BUMP_LEVEL})
+NEXT_VERSION="$(bump ${BUMP_LEVEL})" || true
+
+# Set next version tag in case existing tags not found.
+if [ -z "${NEXT_VERSION}" -a -z "$(git tag)" ]; then
+	case "${BUMP_LEVEL}" in
+		major)
+			NEXT_VERSION="v1.0.0"
+			break
+			;;
+		minor)
+			NEXT_VERSION="v0.1.0"
+			break
+			;;
+		patch)
+			NEXT_VERSION="v0.0.1"
+			break
+			;;
+	esac
+fi
 
 if [ -z "${NEXT_VERSION}" ]; then
   echo "Cannot find next version."
